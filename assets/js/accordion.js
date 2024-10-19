52731805c8fd4e5c0ca17575c737a7f51f7dd284
its-mahdi-dev <dev@itsmahdi.com>
2024-10-19 22:06:29 +0330

    Add header and arrow to accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -3,10 +3,13 @@ document
   .forEach(function (checkbox) {
     checkbox.addEventListener("change", function () {
       const accordionBody = this.nextElementSibling.nextElementSibling;
-
+      const arrow = this.nextElementSibling.getElementsByClassName("arrow")[0];
+      console.log(arrow)
       if (this.checked) {
+        arrow.style.transform = "rotate(180deg)";
         accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
       } else {
+        arrow.style.transform = "rotate(0deg)";
         accordionBody.style.maxHeight = 0;
       }
     });
